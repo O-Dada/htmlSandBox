46661f8d0324d2ae2b12ce6cc55bb83aa10bec17
O-Dada <129070746+O-Dada@users.noreply.github.com>
2023-06-17 12:07:54 +0100

    Update assignmentZero.js

--- javaScriptClassExercises/assignmentZero.js
+++ javaScriptClassExercises/assignmentZero.js
@@ -2,10 +2,10 @@
 
 // Exercise A
 let spaceShuttle = 'Determination',
-shuttleSpeed = 17500, 
-marsDistance = 225000000,
-moonDistance = 384400,
-milesPerKm = 0.621;
+    shuttleSpeed = 17500, 
+    marsDistance = 225000000,
+    moonDistance = 384400,
+    milesPerKm = 0.621;
 
 // Exercise B
 console.log(typeof spaceShuttle);
@@ -16,15 +16,15 @@ console.log(typeof milesPerKm);
 
 //Exercise C
 let marsMiles = marsDistance * milesPerKm,
-marsHours = marsMiles / shuttleSpeed,
-marsDays = marsHours / 24;
+    marsHours = marsMiles / shuttleSpeed,
+    marsDays = marsHours / 24;
 
 // Exercise D
 console.log(spaceShuttle + " " + "will take " + marsDays + " days to reach Mars.");
 
 // Exercise E
 let moonMiles = moonDistance * milesPerKm,
-moonHours = moonMiles / shuttleSpeed,
-moonDays = moonHours / 24;
+    moonHours = moonMiles / shuttleSpeed,
+    moonDays = moonHours / 24;
 
 console.log(spaceShuttle + " " + "will take " + moonDays + " days to reach Mars.");
