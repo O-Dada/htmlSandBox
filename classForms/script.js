08efe625c5dca2864684aa720907c78f685b4507
O-Dada <129070746+O-Dada@users.noreply.github.com>
2023-06-17 11:56:06 +0100

    filesStart

---
+++ classForms/script.js
@@ -0,0 +1,34 @@
+alert("Let's plan a wedding!");
+prompt("How do we go about the wedding?");
+console.log("See line 4 for your mistake");
+console.error("You have made a wrong decision");
+// Single comment
+/* A multi-
+line comment*/
+
+//Variables
+let age = 22;
+var name = "oDada";
+const pi = 3.142;
+let understood = true;
+let nullVariable = null;
+let undefinedVariable = undefined;
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof understood);
+console.log(typeof nullVariable);
+console.log(typeof pi);
+console.log(typeof undefinedVariable);
+
+let x = 2;
+
+console.log(1 + 5 % 3);
+
+console.log(3 > 2 > 1 === false);
+
+console.log(true > 1);
+
+// console.log(7 = 7);
+
+
---
+++ javaScriptClassExercises/assignmentZero.js
@@ -0,0 +1,25 @@
+// 4.96 Exercises
+
+// Exercise A
+let spaceShuttle = 'Determination',
+shuttleSpeed = 17500, 
+marsDistance = 225000000,
+moonDistance = 384400,
+milesPerKm = 0.621;
+
+// Exercise B
+console.log(typeof spaceShuttle);
+console.log(typeof shuttle_speed);
+console.log(typeof marsDistance);
+console.log(typeof moonDistance);
+console.log(typeof milesPerKm);
+
+//Exercise C
+let marsMiles = marsDistance * milesPerKm,
+marsHours = marsMiles / shuttleSpeed,
+marsDays = marsHours / 24;
+
+// Exercise D
+console.log(spaceShuttle + " " + "will take " + marsDays + " days to reach Mars.");
+
+
